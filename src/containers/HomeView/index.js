32d8fd8041fb7f2f3533a9fe618f3359d80fda32
Mark Muskardin <mark.muskardin@gmail.com>
2017-12-18 22:06:51 -0800

    first commit

---
+++ src/containers/HomeView/index.js
@@ -0,0 +1,66 @@
+import React, { Component }   from 'react'
+import { connect }            from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { withRouter }         from 'react-router-dom';
+import { UploadBox }          from 'components/UploadBox'
+import Button                  from 'components/Button'
+
+/* component styles */
+import { styles } from './styles.scss';
+
+/* actions */
+import * as uiActionCreators from 'core/actions/actions-ui';
+
+class HomeView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fileAdded: false
+    }
+  }
+
+  onDrop=() => {
+    this.setState({
+      fileAdded: true
+    })
+  }
+
+  registerAsset=() => {    
+    const { history } = this.props
+    history.push('/register')
+  }
+
+  render() {
+    const { fileAdded } = this.state
+
+    return (
+      <div className={styles}>   
+        <div id="home-view">   
+          <UploadBox onDrop={this.onDrop} />
+            <div className={!fileAdded ? 'opaque' : ''}>
+              <div id="register-actions">
+                <Button 
+                  onTouchTap={this.registerAsset}
+                  label="Register Asset On Blockchain"
+                  raised={true}
+                  className="tertiary" />
+                <a id="reset" href="#">Reset</a>
+              </div>
+            </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      ui: bindActionCreators(uiActionCreators, dispatch)
+    }
+  };
+}
+
+export default withRouter(
+  connect(null, mapDispatchToProps)(HomeView)
+)
