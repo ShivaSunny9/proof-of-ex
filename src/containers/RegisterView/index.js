b334e2c3cc18f861562f6eaab1ed9f05399712b8
Mark Muskardin <mark.muskardin@gmail.com>
2018-02-05 21:57:29 -0800

    first pass at Stepper

--- src/containers/RegisterView/index.js
+++ src/containers/RegisterView/index.js
@@ -8,6 +8,11 @@ import Button                  from 'components/Button'
 import imagePlaceholderSvg     from 'assets/images/image-placeholder.svg' 
 import ProgressIndicator       from 'components/ProgressIndicator'
 import getStyles               from 'core/utils/util-styles'
+import {
+  Step,
+  Stepper,
+  StepLabel
+} from 'material-ui/Stepper';
 
 /* component styles */
 import { styles } from './styles.scss'
@@ -19,13 +24,124 @@ class RegisterView extends Component {
     super(props);
     this.state = {
       mainImage: '',
-      imagePlaceholder: ''
+      imagePlaceholder: '',
+      loading: false,
+      finished: false,
+      stepIndex: 0
     }
   }
 
   componentDidMount() {    
     this.setImage()
     this.showImage()
+  }
+
+  dummyAsync = (cb) => {
+    this.setState({loading: true}, () => {
+      this.asyncTimer = setTimeout(cb, 500);
+    });
+  };
+
+  handleNext = () => {
+    const {stepIndex} = this.state;
+    if (!this.state.loading) {
+      this.dummyAsync(() => this.setState({
+        loading: false,
+        stepIndex: stepIndex + 1,
+        finished: stepIndex >= 2
+      }));
+    }
+  };
+
+  handlePrev = () => {
+    const {stepIndex} = this.state;
+    if (!this.state.loading) {
+      this.dummyAsync(() => this.setState({
+        loading: false,
+        stepIndex: stepIndex - 1
+      }));
+    }
+  };
+
+  renderContent() {
+    const {finished, stepIndex} = this.state;
+    const contentStyle = {margin: '0 16px', overflow: 'hidden'};
+
+    if (finished) {
+      return (
+        <div style={contentStyle}>
+          <p>
+            <a
+              href="#"
+              onClick={(event) => {
+                event.preventDefault();
+                this.setState({stepIndex: 0, finished: false});
+              }}
+            >
+              Click here
+            </a> to reset the example.
+          </p>
+        </div>
+      );
+    }
+    return (
+      <div style={contentStyle}>
+        <div>{this.getStepContent(stepIndex)}</div>
+        <div id="button-controls">
+          <Button
+            type="raised"
+            label={stepIndex === 2 ? 'Pay & Confirm' : 'Next'}
+            primary={true}
+            onClick={this.handleNext}
+          />
+          <Button
+            type="flat"
+            label="Back"
+            secondary={true}
+            disabled={stepIndex === 0}
+            onClick={this.handlePrev}
+            style={{marginRight: 12}}
+          />
+        </div>
+      </div>
+    );
+  }
+
+  getStepContent(stepIndex) {
+    switch (stepIndex) {
+    case 0:
+      return (
+          <div id="registration-form">
+            <h2>Step 1 - Enter Your Credentials</h2>
+            <span>Your email address and public key are registered on the Blockchain</span>
+            <Form>
+              <Label text="Your Email Address" />
+              <Input type="text" />
+              <Label text="Your Ethereum Wallet Address (Public Key)" />
+              <Input type="text" />
+            </Form>
+          </div>)
+    case 1:
+      return (
+          <div id="registration-form">
+            <h2>Step 2 - Create A Unique Hash Of Your Asset</h2>
+            <span>The SHA256 algorithm is used to create a unique fingerprint of your asset</span>
+            <Form>
+              <Label text="Your Asset's Unique Hash" />
+              <Input type="text" value={"2sdlkjfghl345345lkjh"} disabled/>
+            </Form>
+          </div>)
+    case 2:
+      return (
+          <div id="registration-form">
+            <h2>Step 3 - Pay Gas & Confirm Transaction</h2>
+            <span>You need to pay gas in Ether in order to create a record on the Blockchain</span>
+            <Form>
+              <Label text="Estimated Gas" />
+              <Input type="text" value={123123123} disabled/>
+            </Form>
+          </div>)
+    }
   }
 
   setImage=() => {
@@ -61,40 +177,35 @@ class RegisterView extends Component {
 
   render() {
     const { imagePlaceholder } = this.state
+    const { stepIndex} = this.state;
 
     return (
       <div className={styles}> 
         <div id="register-view">   
           <div id="image-container">
-            <Paper zDepth={2}>
+            <Paper zDepth={1}>
               <div id="image-preview">
                 {imagePlaceholder}
               </div>
             </Paper>
           </div>
           <div>
-            <Paper zDepth={2} id="registration-form-container">
-              <div id="registration-form">
-              <h2>Register Your Digital Asset</h2>
-              <span>Create a permanent record of your asset on the Blockchain</span>
-              <Form>
-                <Label text="Your Email Address" />
-                <Input type="text" />
-                <Label text="Your Ethereum Wallet Address (Public Key)" />
-                <Input type="text" />
-                <Label text="Your Asset's Unique Hash" />
-                <Input type="text" value={"2sdlkjfghl345345lkjh"} disabled/>
-                <Label text="Estimated Gas" />
-                <Input type="text" value={123123123} disabled/>
-                <Button
-                  label="Next"
-                  labelPosition="before"
-                  type="raised"
-                  primary={true}
-                />
-              </Form>
-              </div>
-            </Paper>
+            <div id="registration-form-container">
+              <Stepper activeStep={stepIndex}>
+              <Step>
+                <StepLabel>Enter your credentials</StepLabel>
+              </Step>
+              <Step>
+                <StepLabel>Generate a unique hash</StepLabel>
+              </Step>
+              <Step>
+                <StepLabel>Pay Gas & Confirm</StepLabel>
+              </Step>
+              </Stepper>
+
+              {this.renderContent()}
+
+            </div>
           </div>
         </div>
       </div>
