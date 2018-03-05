c9113fd5508a956edbaac385dba6506129899db3
Mark Muskardin <mark.muskardin@gmail.com>
2018-03-05 11:37:30 -0800

    code clean up

--- src/core/types.js
+++ src/core/types.js
@@ -1,15 +1,15 @@
 import keyMirror from 'keymirror';
 
 export default keyMirror({
-  OPEN_LEFT_NAV      : null,
-  CLOSE_LEFT_NAV     : null,
-  OPEN_RIGHT_NAV     : null,
-  CLOSE_RIGHT_NAV    : null,
-  CLEAR_UI           : null,
-  SHOW_MODAL         : null,
-  CLOSE_MODAL        : null,
-  SET_PROVIDER       : null,
-  ADD_ASSET          : null,
-  CHECK_ASSET        : null,
-  CREATE_ASSET_HASH  : null
+  OPEN_LEFT_NAV     : null,
+  CLOSE_LEFT_NAV    : null,
+  OPEN_RIGHT_NAV    : null,
+  CLOSE_RIGHT_NAV   : null,
+  CLEAR_UI          : null,
+  SHOW_MODAL        : null,
+  CLOSE_MODAL       : null,
+  SET_PROVIDER      : null,
+  ADD_ASSET         : null,
+  CHECK_ASSET       : null,
+  CREATE_ASSET_HASH : null
 });
